7e065c6368916012a0327bc388a527fb53aed013
ZainAsif767 <zainclasher2002@gmail.com>
2023-07-30 17:16:31 +0500

    customButton

---
+++ components/CustomButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { CustomButtonProps } from "@/types";
+import Image from "next/image"
+
+const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+  return (
+    <button
+        disabled={false}
+        type={"button"}
+        className={`custom-btn ${containerStyles}`}
+        onClick={handleClick}
+    >
+       <span className={`flex-1`}>
+        {title}
+      </span>  
+    </button>
+  )
+}
+
+export default CustomButton  
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 import Hero from "./Hero";
+import CustomButton from "./CustomButton";
 
-export { Hero };
+export { Hero, CustomButton };
