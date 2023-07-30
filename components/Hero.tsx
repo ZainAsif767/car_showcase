72b0a4b697ce816d013ef90cef4cf3c7bd2f046e
ZainAsif767 <zainclasher2002@gmail.com>
2023-07-30 21:59:30 +0500

    fixed image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,9 +30,8 @@ const Hero = () => {
         <div className="hero__image">
           <Image src="/hero.png" alt="hero"
           fill className="object-contain" />
-
-          <div className="hero__image-overlay" />
         </div>
+      <div className="hero__image-overlay" />
       </div>
     </div>
   )
