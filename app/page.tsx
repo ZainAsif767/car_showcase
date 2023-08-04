a0762e5a8e8ed5f298cff8bbd0120335ee4d58cc
ZainAsif767 <zainclasher2002@gmail.com>
2023-08-04 23:19:11 +0500

    API fetch in Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,36 @@
-import { Hero, SearchBar, CustomFilter } from '@/components'
-import Image from 'next/image'
+import { Hero, SearchBar, CustomFilter } from "@/components";
+import { fetchCars } from "@/utils";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  console.log(allCars);
   return (
     <main className="overflow-hidden">
       <Hero />
 
-      <div className='mt-12 padding-x
-        padding-y max-width' id='discover'>
-        <div className='home__text-container'>
-          <h1 className="text-4xl
-           font-extrabold">
-              Car Catalogue
+      <div
+        className="mt-12 padding-x
+        padding-y max-width"
+        id="discover"
+      >
+        <div className="home__text-container">
+          <h1
+            className="text-4xl
+           font-extrabold"
+          >
+            Car Catalogue
           </h1>
-           <p>Explore the cars you might like</p>
+          <p>Explore the cars you might like</p>
         </div>
-        <div className='home__filters'>
+        <div className="home__filters">
           <SearchBar />
-         <div className='home__filter-container'>
+          <div className="home__filter-container">
             <CustomFilter title="fuel" />
             <CustomFilter title="year" />
-         </div>
-       </div>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
