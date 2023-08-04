398d4ab3b3f98d5846ba869290864e45a4f0a0b0
ZainAsif767 <zainclasher2002@gmail.com>
2023-08-05 00:30:10 +0500

    CarCard and CarDetails

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
 export { 
     Hero,
@@ -13,5 +15,7 @@ export {
     Footer,
     SearchBar,
     CustomFilter,
-    SearchManufacturer
+    SearchManufacturer,
+    CarCard,
+    CarDetails
 };
