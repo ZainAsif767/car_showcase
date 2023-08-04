5157f38ef527d5ec496c8837bc62d2f9df44b50a
ZainAsif767 <zainclasher2002@gmail.com>
2023-08-05 00:30:25 +0500

    types includes

--- types/index.ts
+++ types/index.ts
@@ -6,9 +6,27 @@ export interface CustomButtonProps {
     handleClick?:
     MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg:number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year:number;
 }
