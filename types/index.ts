515f022c2d0a1070f3ae045a2c6aa79909e859c4
ZainAsif767 <zainclasher2002@gmail.com>
2023-08-10 16:49:53 +0500

    HomeProps, FilterProps,OptionProps,CustomProps

--- types/index.ts
+++ types/index.ts
@@ -30,3 +30,25 @@ export interface CarProps {
     transmission: string;
     year:number;
 }
+
+export interface HomeProps {
+    searchParams: FilterProps;
+  }
+  
+
+export interface FilterProps {
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+  }
