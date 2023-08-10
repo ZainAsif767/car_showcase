2d3f06f254432409d4d90771859730965639a4a3
ZainAsif767 <zainclasher2002@gmail.com>
2023-08-10 16:49:10 +0500

    Searchbar

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+"use client";
 
-interface CustomFilterProps {
-  title: string;
-}
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Listbox, Transition } from "@headlessui/react";
+import { CustomFilterProps } from "@/types";
 
-const CustomFilter = ({ title }: CustomFilterProps) => {
-  return <div>CustomFilter</div>;
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div className="w-fit">
+      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              alt="chevron up down"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+            />
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
+  );
 };
 
 export default CustomFilter;
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,25 +1,89 @@
 "use client";
 
-import React from 'react'
-import { useState } from 'react';
-import { SearchManufacturer } from '.';
+import React from "react";
+import { useState } from "react";
+import Image from "next/image";
+import { SearchManufacturer } from ".";
+import { useRouter } from "next/navigation";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState('');
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+  const router = useRouter();
 
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the Search bar");
+    }
 
-  const handleSearch = () => {}
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
 
   return (
-    <form className='searchbar' onSubmit={handleSearch}>
-        <div className='searchbar__item'>
-            <SearchManufacturer
-                manufacturer={manufacturer}
-                setManufacturer={setManufacturer}
-            />
-        </div>
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          alt="car model"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
